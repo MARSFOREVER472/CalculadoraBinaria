5012f6fec1dbe22806b34fe317cb59c0177e3a2d
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-05-29 12:13:28 -0400

    Variables creadas dinámicamente al formulario.

--- Algoritmos_Binarios/Form1.cs
+++ Algoritmos_Binarios/Form1.cs
@@ -2,6 +2,17 @@ namespace Algoritmos_Binarios
 {
     public partial class Form1 : Form
     {
+        // VAMOS A CREAR VARIABLES PARA EL FORMULARIO DE MANERA DINÁMICA AQUÍ!!!!...
+
+        int[] valoresBinarios = { 1, 2, 4, 8, 16, 32, 64, 128, 256 }; // VALORES BINARIOS POR CADA 8 BYTES.
+        Random aleatorio = new Random(); // VARIABLE ALEATORIA.
+        List<ComboBox> cajasDeValoresBinarios = new List<ComboBox>(); // VALORES BINARIOS EN CADA CUADRO.
+        int total; // VALOR TOTAL BINARIO.
+        int preguntaNumerica; // NÚMERO POR PREGUNTAS.
+        Label totalTexto = new Label(); // CANTIDAD TOTAL MEDIANTE CADENA DE TEXTO.
+        Label pregunta = new Label(); // PREGUNTA MEDIANTE CADENA DE TEXTO.
+        Label encabezado = new Label(); // ENCABEZADO DEL FORMULARIO MEDIANTE CADENA DE TEXTO.
+
         public Form1()
         {
             InitializeComponent();
